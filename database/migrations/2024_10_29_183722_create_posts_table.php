05b207b00acd583635811d3810cd923618e9a7af
d-yjc <cho.yj.daniel@gmail.com>
2024-12-05 10:08:30 +0000

    Fixed a bug where Livewire would not load because of whitespace in the Post migrations files.

--- database/migrations/2024_10_29_183722_create_posts_table.php
+++ database/migrations/2024_10_29_183722_create_posts_table.php
@@ -1,35 +1,34 @@
-            <?php
+<?php
 
-            use Illuminate\Database\Migrations\Migration;
-            use Illuminate\Database\Schema\Blueprint;
-            use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-            return new class extends Migration
-            {
-                /**
-                 * Run the migrations.  
-                 */
-                public function up(): void
-                {
-                    Schema::create('posts', function (Blueprint $table) {
-                        $table->id();
-                        //foreign ref to user's id  
-                        $table->string('topic');
-                        $table->string('body');
+return new class extends Migration {
+    /**
+     * Run the migrations.  
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            //foreign ref to user's id  
+            $table->string('topic');
+            $table->string('body');
 
-                        $table->bigInteger('chat_user_id')->unsigned();
-                        $table->foreign('chat_user_id')->references('id')->on('chat_users')
-                            ->onDelete('cascade')->onUpdate('cascade');
-                            
-                        $table->timestamps();
-                    });
-                }
+            $table->bigInteger('chat_user_id')->unsigned();
+            $table->foreign('chat_user_id')->references('id')->on('chat_users')
+                ->onDelete('cascade')->onUpdate('cascade');
 
-                /**
-                * Reverse the migrations.
-                */
-                public function down(): void
-                {
-                    Schema::dropIfExists('posts');
-                }
-            };
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
